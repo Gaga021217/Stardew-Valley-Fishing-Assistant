0e8924820090b46fc67a6b0ab9e39ff84204d1c4
Gyorgyevics Gabriella <73666503+Gaga021217@users.noreply.github.com>
2024-03-27 18:08:10 +0100

    Minor fixes
    Fixed time properties in case there's multiple periods to catch (like albacore)
    Added isAvailable for multiple locations. removed now obsolete single location version
    Now checking whether the time given is reasonable
    Reworked printFishDetails to print only information required in argument
    Removed now obsolete printFish method
    Updated constructor to fit the new list of properties
    Moved enumToString methods to EnumHelper

--- Fish.hpp
+++ Fish.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+
 #include "Enums.hpp"
 
 
@@ -20,13 +21,13 @@ using namespace std;
 
 class Fish {
 public:
-    string getName() { 
-        return Name; 
+    string getName() {
+        return Name;
     }
-    string getDescription() { 
-        return Description; 
+    string getDescription() {
+        return Description;
     }
-    string getPrice(double fisher) { 
+    string getPrice(double fisher) {
         string str = "";
         str.append("Normal: ");
         str.append(to_string(int(floor(Price[0] * fisher))));
@@ -37,54 +38,67 @@ public:
         str.append("\n\t\tIridium: ");
         str.append(to_string(int(floor(Price[3] * fisher))));
         return str;
-        
+
     }
-    string getLocations() { 
+    string getLocations() {
         string str = "";
         for (int i = 0; i < Locations.size(); i++) {
-            str.append(locationToString(Locations[i]));
+            str.append(eh.locationToString(Locations[i]));
             if (i != Locations.size() - 1) {
                 str.append(", ");
             }
         }
-        
+
 
         return str;
     }
+    string getNote() {
+        return Note;
+    }
     string getAvailableTime() {
         string str = "";
-        if(StartTime == 6 && EndTime == 26) {
+        if (StartTime[0] == 6 && EndTime[0] == 26) {
             str.append("All Day");
             return str;
         }
-        str.append(to_string(StartTime));
-        str.append(" - ");
-        if(EndTime == 26) {
-            str.append("2");
-        } else if(EndTime == 25) {
-            str.append("1");
-        } else if(EndTime == 24) {
-            str.append("0");
-        }else {
-            str.append(to_string(EndTime));
+        for (int i = 0; i < StartTime.size(); i++) {
+            str.append(to_string(StartTime[i]));
+            str.append(".00 - ");
+            if (EndTime[i] == 26) {
+                str.append("2");
+            }
+            else if (EndTime[i] == 25) {
+                str.append("1");
+            }
+            else if (EndTime[i] == 24) {
+                str.append("0");
+            }
+            else {
+                str.append(to_string(EndTime[i]));
+            }
+            str.append(".00");
+            if (i != StartTime.size() - 1) {
+                str.append("; ");
+            }
+
         }
         return str;
     }
-    string getSeasons() { 
+    string getSeasons() {
         string str = "";
-        if(Seasons.size() == 4) {
+        if (Seasons.size() == 4) {
             str.append("All Seasons");
             return str;
         }
         for (int i = 0; i < Seasons.size(); i++) {
-            str.append(seasonToString(Seasons[i]));
+            str.append(eh.seasonToString(Seasons[i]));
             if (i != Seasons.size() - 1) {
                 str.append(", ");
             }
         }
         return str;
     }
-    string getWeathers() { 
+    string getWeathers() {
         string str = "";
         if (Rain && !Sun) {
             str.append("Rainy");
@@ -98,7 +112,7 @@ public:
 
         return str;
     }
-    string getSizeRange() { 
+    string getSizeRange() {
         string str = "";
         str.append(to_string(MinSize));
         str.append(" - ");
@@ -109,13 +123,13 @@ public:
         string str = "";
         str.append(to_string(Difficulty));
         str.append(" ");
-        str.append(behaviorToString(Behave));
+        str.append(eh.behaviorToString(Behave));
         return str;
     }
-    int getXP() { 
-        return XP; 
+    int getXP() {
+        return XP;
     }
-    string getUsedIn() { 
+    string getUsedIn() {
         string str = "";
         for (int i = 0; i < UsedIn.size(); i++) {
             str.append(UsedIn[i]);
@@ -126,277 +140,166 @@ public:
         return str;
     }
 
+    //Returns whether the fish is available in given season
     bool isAvailable(Season season) {
         for (int i = 0; i < Seasons.size(); i++) {
-            if(Seasons[i] == season) {
+            if (Seasons[i] == season) {
                 return true;
             }
         }
         return false;
     }
-
-    bool isAvailable(Location loc) {
-        for (int i = 0; i < Locations.size(); i++) {
-            if(Locations[i] == loc) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool isAvailable(bool rain, bool sun) {
-        if(Rain == rain || Sun == sun) {
-            return true;
-        }
-        return false;
-    }
-
-    bool isAvailable(int time) {
-        if(time < 6 || time > 26) {
-            return true;
-        }
-        if(StartTime == 6 && EndTime == 26) {
-            return true;
-        }
-        if(time >= StartTime && time < EndTime) {
-            return true;
-        }
-
-        return false;
-    }
-
-
-
-    void printFish() {
-        string str;
-        str.append("\n---------------------------------------------\nName: ");
-        str.append(getName());
-        str.append("\nCatch between: ");
-        str.append(getAvailableTime());
-        str.append("\nIn weathers: ");
-        str.append(getWeathers());
-            
-        str.append("\nLocations:");
-        str.append(getLocations());
-        
-        str.append("\nIn season: ");
-        str.append(getSeasons());
-
-        if(SpecialSeasons.size() != 0) {
-            str.append("\nOr, catch at ");
-            str.append(locationToString(SpecialLocation));
-            str.append(" in ");
-            if(SpecialSeasons.size() == 4) {
-                str.append("any season.");
-            }
-            else {
-                for (int i = 0; i < SpecialSeasons.size(); i++) {
-                    str.append(seasonToString(SpecialSeasons[i]));
-                    if (i != Seasons.size() - 1) {
-                        str.append(", ");
-                    }
+    //Returns whether the fish is available at a given location
+    bool isAvailable(vector<string> locs) {
+        for (int i = 0; i < locs.size(); i++) {
+            for (int j = 0; j < Locations.size(); j++) {
+                if (eh.stringToLocation(locs[i]) == Locations[j]) {
+                    return true;
                 }
             }
         }
-         
-        str.append("\nDifficulty: ");
-        str.append(getDifficulty());
-        str.append("\nYou can use it for: ");
-        str.append(getUsedIn());
-        str.append("\n---------------------------------------------\n");
-        cout << str;
+        return false;
+    }
+    //Returns whether the fish is available in a given weather
+    bool isAvailable(bool rain, bool sun) {
+        if (Rain == rain || Sun == sun) {
+            return true;
+        }
+        return false;
+    }
+    //Returns whether the fish is available at a given time
+    bool isAvailable(int time) {
+        if (time < 6 || time > 26) {
+            return true;
+        }
+        if (StartTime[0] == 6 && EndTime[0] == 26) {
+            return true;
+        }
+        if (time >= StartTime[0] && time < EndTime[0]) {
+            return true;
+        }
+        if (StartTime.size() > 1) {
+            if (time >= StartTime[1] && time < EndTime[1]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    void printFishAllDetails(double fisher) {
+
+    void printFishDetails(vector<string> properties, double fisher) {
+        // /*debug*/cout << properties.size();
+
         string str;
         cout << "\n---------------------------------------------\n";
-        cout << YELLOW << "Name:\t\t" << RESET << getName()  << endl;
-        cout << YELLOW << "Description:\t" << RESET << getDescription() << endl;
-        cout << YELLOW << "Price:\t\t" << RESET << getPrice(fisher) << endl;
-        cout << YELLOW << "Catch between:\t" << RESET << getAvailableTime() << endl;
-        cout << YELLOW << "In weathers:\t" << RESET << getWeathers() << endl;
-        cout << YELLOW << "Locations:\t" << RESET << getLocations() << endl;
-        cout << YELLOW << "In season:\t" << RESET << getSeasons() << endl;
-
-
-        if(SpecialLocation != Location::Unknown) {
-            cout << YELLOW << "Or, catch at\t" << RESET << locationToString(SpecialLocation) << " in ";
-            if(SpecialSeasons.size() == 4) {
-                cout << "any season." << endl;
-            }
-            else {
-                for (int i = 0; i < SpecialSeasons.size(); i++) {
-                    cout << seasonToString(SpecialSeasons[i]);
-                    if (i != Seasons.size() - 1) {
-                        cout << ", " << endl;
-                    }
-                }
-            }
+        cout << YELLOW << "Name:\t\t" << RESET << getName() << endl;
+        if (properties[0] == "Description") {
+            cout << YELLOW << "Description:\t" << RESET << getDescription() << endl;
+            properties.erase(properties.begin());
         }
-        
-        cout << YELLOW << "Size:\t\t" << RESET << getSizeRange() << endl;
-        cout << YELLOW << "Difficulty:\t" << RESET << getDifficulty() << endl;
-        cout << YELLOW << "XP:\t\t" << RESET << getXP() << endl;
-        cout << YELLOW << "Use it for:\t" << RESET << getUsedIn() << endl;
+        if ((properties.size() > 0) && (properties[0] == "Price")) {
+            cout << YELLOW << "Price:\t\t" << RESET << getPrice(fisher) << endl;
+            properties.erase(properties.begin());
+        }
+        if (properties[0] == "Locations") {
+            cout << YELLOW << "Locations:\t" << RESET << getLocations() << endl;
+            properties.erase(properties.begin());
+            
+        }
+        if(Note != "") {
+            cout << YELLOW << "Note:\t\t" << RESET << getNote() << endl;
+        }
+        if (properties[0] == "Time") {
+            cout << YELLOW << "Catch between:\t" << RESET << getAvailableTime() << endl;
+            properties.erase(properties.begin());
+        }
+        if (properties[0] == "Season") {
+            cout << YELLOW << "In season:\t" << RESET << getSeasons() << endl;
+            properties.erase(properties.begin());
+        }
+        if (properties[0] == "Weather") {
+            cout << YELLOW << "In weathers:\t" << RESET << getWeathers() << endl;
+            properties.erase(properties.begin());
+        }
+        if (properties[0] == "Size") {
+            cout << YELLOW << "Size:\t\t" << RESET << getSizeRange() << endl;
+            properties.erase(properties.begin());
+        }
+        if (properties[0] == "Difficulty") {
+            cout << YELLOW << "Difficulty:\t" << RESET << getDifficulty() << endl;
+            properties.erase(properties.begin());
+        }
+        if (properties[0] == "XP") {
+            cout << YELLOW << "XP:\t\t" << RESET << getXP() << endl;
+            properties.erase(properties.begin());
+        }
+        if (properties[0] == "Uses") {
+            cout << YELLOW << "Use it for:\t" << RESET << getUsedIn() << endl;
+            properties.erase(properties.begin());
+        }
+
         cout << "---------------------------------------------\n";
     }
 
-    Fish(string name, string description, vector<int> price, vector<Location> locations, int startTime, int endTime, vector<Season> seasons, vector<Season> specialSeasons, Location specialLocation, bool rain, bool sun, int minSize, int maxSize, int difficulty, Behavior behave, int xp, vector<string> usedIn) {
-            Name = name;
-            Description = description;
-            Price = price;
-            Locations = locations;
-            StartTime = startTime;
-            EndTime = endTime;
-            Seasons = seasons;
-            SpecialSeasons = specialSeasons;
-            SpecialLocation = specialLocation;
-            Rain = rain;
-            Sun = sun;
-            MinSize = minSize;
-            MaxSize = maxSize;
-            Difficulty = difficulty;
-            Behave = behave;
-            XP = xp;
-            UsedIn = usedIn;
-        }
+
+
+
+
+    //Constructor for a fish
+    //String - name of the fish.
+    //String - description of the fish.
+    //Vector<int> - price of the fish, in order: normal, silver, gold, iridium.
+    //Vector<Location> - locations where the fish can be caught.
+    //Int - When does the fish appear?
+    //Int - When does the fish disappear?
+    //Vector<Season> Seasons when the fish can be caught
+    //Bool - Does this fish have special availability
+    //Bool - Can this fish be caught in rainy weather
+    //Bool - Can this fish be caught in sunny weather
+    //Int - Smallest this fish can be
+    //Int - Biggest this fish can be
+    //Int - Difficulty level of this fish
+    //Behavior - Behavior type of the fish
+    //Int - how much XP you gain for catching the fish
+    //Vector<string> - uses of the fish
+    Fish(string name, string description, vector<int> price, vector<Location> locations, string note, vector<int> startTime, vector<int> endTime, vector<Season> seasons, bool rain, bool sun, int minSize, int maxSize, int difficulty, Behavior behave, int xp, vector<string> usedIn) {
+        Name = name;
+        Description = description;
+        Price = price;
+        Locations = locations;
+        Note = note;
+        StartTime = startTime;
+        EndTime = endTime;
+        Seasons = seasons;
+        Rain = rain;
+        Sun = sun;
+        MinSize = minSize;
+        MaxSize = maxSize;
+        Difficulty = difficulty;
+        Behave = behave;
+        XP = xp;
+        UsedIn = usedIn;
+    }
 
 private:
-    string Name;   //name of the fish
-    string Description; //description of the fish
-    vector<int> Price; //prices of the fish in order: Basic, Silver, Gold, Iridium
-    vector<Location> Locations; //locations where the fish can be caught
-    int StartTime; //start time of when the fish can be caught
-    int EndTime; //end time of when the fish can be caught
-    vector<Season> Seasons; //seasons when the fish is available
-    vector<Season> SpecialSeasons; //special seasons when the fish is available
-    Location SpecialLocation; //special location where the fish is available
-    bool Rain; // whether you can catch it in rainy weather
-    bool Sun; // whether you can catch it in sunny weather
-    int MinSize; //minimum size of the fish
-    int MaxSize; //maximum size of the fish
-    int Difficulty; //difficulty of the fish
-    Behavior Behave; //behavior type of the fish
-    int XP; //experience points for catching the fish
-    vector<string> UsedIn; //what recipes the fish is used in
+    string Name;   // Name of the fish
+    string Description; // Description of the fish
+    vector<int> Price; // Prices of the fish in order: Basic, Silver, Gold, Iridium
+    vector<Location> Locations; // Locations where the fish can be caught
+    string Note; // Note about the fish; might be empty
+    vector<int> StartTime; // Start time of when the fish can be caught
+    vector<int> EndTime; // End time of when the fish can be caught
+    vector<Season> Seasons; // Seasons when the fish is available
+    bool Rain; // Whether you can catch it in rainy weather
+    bool Sun; // Whether you can catch it in sunny weather
+    int MinSize; // Minimum size of the fish
+    int MaxSize; // Maximum size of the fish
+    int Difficulty; // Difficulty of the fish
+    Behavior Behave; // Behavior type of the fish
+    int XP; // Experience points for catching the fish
+    vector<string> UsedIn; // What recipes the fish is used in
 
-
-    string locationToString(Location loc) {
-        string str = "";
-        switch (loc)
-        {
-            case Location::ForestFarm:
-                return "Forest Farm";
-                break;
-            case Location::Ocean:
-                return "Ocean";
-                break;
-            case Location::CindersapForestPond:
-                return "Cindersap Forest Pond";
-                break;
-            case Location::TownRiver:
-                return "Town River";
-                break;
-            case Location::ForestRiver:
-                return "Forest River";
-                break;
-            case Location::MountainLake:
-                return "Mountain Lake";
-                break;
-            case Location::ForestPond: 
-                return "Forest Pond";
-                break;
-            case Location::SecretWoods:
-                return "Secret Woods";
-                break;
-            case Location::Mines:
-                return "Mines";
-                break; 
-            case Location::WitchSwamp:
-                return "Witch Swamp";
-                break; 
-            case Location::Sewers:
-                return "The Sewers";
-                break;
-            case Location::MutantBugLair:
-                return "Mutant Bug Lair";
-                break;
-            case Location::Desert:
-                return "The Desert";
-                break;
-            case Location::PirateCove:
-                return "Pirate Cove";
-                break;
-            case Location::GIPond:
-                return "Ginger Island Pond";
-                break;
-            case Location::GIRiver:
-                return "Ginger Island River";
-                break;
-            case Location::GIOcean:
-                return "Ginger Island Ocean";
-                break;
-            case Location::VolcanoCaldera:
-                return "Volcano Caldera";
-                break;
-            case Location::NightMarket:
-                return "Night Market";
-                break;
-            default:
-                return "Unknown";
-                break;
-        }
-        return str;
-    }
-
-    string seasonToString(Season season) {
-        string str = "";
-        switch (season)
-        {
-            case Season::Spring:
-                return "Spring";
-                break;
-            case Season::Summer:
-                return "Summer";
-                break;
-            case Season::Fall:
-                return "Fall";
-                break;
-            case Season::Winter:
-                return "Winter";
-                break;
-            default:
-                return "Unknown";
-                break;
-        }
-        return str;
-    }
-
-    string behaviorToString(Behavior behavior) {
-        string str = "";
-        switch (behavior)
-        {
-            case Behavior::Floater:
-                return "Floater";
-                break;
-            case Behavior::Mixed:
-                return "Mixed";
-                break;
-            case Behavior::Smooth:
-                return "Smooth";
-                break;
-            case Behavior::Sinker:
-                return "Sinker";
-                break;
-            case Behavior::Dart:
-                return "Dart";
-                break;
-            default:
-                return "Unkown";
-                break;
-        }
-    }
-
-
+    EnumHelper eh;
 
 };
