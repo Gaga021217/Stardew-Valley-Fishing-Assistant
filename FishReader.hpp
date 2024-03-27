bd4e610fa9b1ae12634f32d6b535eaaae699379b
Gyorgyevics Gabriella <73666503+Gaga021217@users.noreply.github.com>
2024-03-27 18:10:06 +0100

    Restructuring
    Moved stringToEnum methods to EnumHelper class
    Moved file reading (from readAll) to getFishList (instead of an unnecessary forwarding)
    Updated readFish to the new file structure and constructor list

--- FishReader.hpp
+++ FishReader.hpp
@@ -3,130 +3,108 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+
 #include "Fish.hpp"
 
 using namespace std;
 
 class FishReader {
 public:
-    FishReader(string path) {
-        path = path;
-        readAll();
-    }
-    
-    vector<Fish> getFishList() {
+    FishReader() {}
+
+    vector<Fish> getFishList(string path) {
+        ifstream file;
+
+        vector<Fish> fishList;
+
+        file.open(path);
+
+        if (!file.is_open()) {
+            cout << "Error opening file!" << endl;
+            return fishList;
+        }
+
+        string line;
+        while (getline(file, line)) {
+            Fish fsh = readFish(line);
+            fishList.push_back(fsh);
+        }
+
+        file.close();
+
+
         return fishList;
     }
 
-     
-    Location stringToLocation(string str) {
-        if(str == "ForestFarm") { return Location::ForestFarm; }
-        if(str == "Ocean") { return Location::Ocean; }
-        if(str == "CindersapForestPond") { return Location::CindersapForestPond; }
-        if(str == "TownRiver") { return Location::TownRiver; }
-        if(str == "ForestRiver") { return Location::ForestRiver; }
-        if(str == "MountainLake") { return Location::MountainLake; }
-        if(str == "ForestPond") { return Location::ForestPond; }
-        if(str == "SecretWoods") { return Location::SecretWoods; }
-        if(str == "Mines") { return Location::Mines; }
-        if(str == "WitchSwamp") { return Location::WitchSwamp; }
-        if(str == "Sewers") { return Location::Sewers; }
-        if(str == "MutantBugLair") { return Location::MutantBugLair; }
-        if(str == "Desert") { return Location::Desert; }
-        if(str == "PirateCove") { return Location::PirateCove; }
-        if(str == "GIPond") { return Location::GIPond; }
-        if(str == "GIRiver") { return Location::GIRiver; }
-        if(str == "GIOcean") { return Location::GIOcean; }
-        if(str == "VolcanoCaldera") { return Location::VolcanoCaldera; }
-        if(str == "NightMarket") { return Location::NightMarket; }
-        return Location::Unknown;
-    }
-
-
-
-    Behavior  stringToBehavior(string str) {
-        if(str == "Floater") { return Behavior::Floater; }
-        if(str == "Mixed") { return Behavior::Mixed; }
-        if(str == "Smooth") { return Behavior::Smooth; }
-        if(str == "Sinker") { return Behavior::Sinker; }
-        if(str == "Dart") { return Behavior::Dart; }
-        return Behavior::Unknown;
-    }
-
-    Season stringToSeason(string str) {
-        if(str == "Spring") { return Season::Spring; }
-        if(str == "Summer") { return Season::Summer; }
-        if(str == "Fall") { return Season::Fall; }
-        if(str == "Winter") { return Season::Winter; }
-        return Season::Unknown;
-    }
-
 private:
-    string path;
-    vector<Fish> fishList;
-
+    EnumHelper eh;
+    
     Fish readFish(string line) {
-        //Pufferfish;Inflates when threatened.;200,250,300,400;Ocean,GIOcean;12;16;Summer;Spring,Summer,Fall,Winter;GIOcean;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
+        
+        //Pufferfish;Inflates when threatened.;200,250,300,400;Ocean,GIOcean;;12;16;Summer;true;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
         string name = line.substr(0, line.find(";"));
         line = line.substr(line.find(";") + 1);
-        //Inflates when threatened.;200,250,300,400;Ocean,GIOcean;12;16;Summer;Spring,Summer,Fall,Winter;GIOcean;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
 
+
+        //Inflates when threatened.;200,250,300,400;Ocean,GIOcean;;12;16;Summer;true;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
         string description = line.substr(0, line.find(";"));
         line = line.substr(line.find(";") + 1);
-        //200,250,300,400;Ocean,GIOcean;12;16;Summer;Spring,Summer,Fall,Winter;GIOcean;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
 
+        //200,250,300,400;Ocean,GIOcean;;12;16;Summer;true;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
         vector<int> price;
         string temp = line.substr(0, line.find(";"));
-        temp.append(",");
         line = line.substr(line.find(";") + 1);
-        while(temp.find(',') != string::npos) {
+        temp.append(",");
+        while (temp.find(',') != string::npos) {
             price.push_back(stoi(temp.substr(0, temp.find(","))));
             temp = temp.substr(temp.find(",") + 1);
         }
-        //Ocean,GIOcean;12;16;Summer;Spring,Summer,Fall,Winter;GIOcean;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
 
+        //Ocean,GIOcean;;12;16;Summer;true;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
         vector<Location> locations;
         temp = line.substr(0, line.find(";"));
         line = line.substr(line.find(";") + 1);
         temp.append(",");
-        while (temp.find(',') != string::npos) { 
-            locations.push_back(stringToLocation(temp.substr(0, temp.find(","))));
+        while (temp.find(',') != string::npos) {
+            locations.push_back(eh.stringToLocation(temp.substr(0, temp.find(","))));
             temp = temp.substr(temp.find(",") + 1);
         }
-        //12;16;Summer;Spring,Summer,Fall,Winter;GIOcean;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
 
-        int startTime = stoi(line.substr(0, line.find(";")));
+        //;12;16;Summer;true;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
+        string note = line.substr(0, line.find(";"));
         line = line.substr(line.find(";") + 1);
-        //16;Summer;Spring,Summer,Fall,Winter;GIOcean;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
 
-        int endTime = stoi(line.substr(0, line.find(";")));
+        //12;16;Summer;true;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
+        vector<int> startTime;
+        temp = line.substr(0, line.find(";"));
+        temp.append(",");
+        while(temp.find(',') != string::npos) {
+            startTime.push_back(stoi(temp.substr(0, temp.find(","))));
+            temp = temp.substr(temp.find(",") + 1);
+        }
         line = line.substr(line.find(";") + 1);
-        //Summer;Spring,Summer,Fall,Winter;GIOcean;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
 
+        //16;Summer;true;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
+        vector<int> endTime;
+        temp = line.substr(0, line.find(";"));
+        temp.append(",");
+        while(temp.find(',') != string::npos) {
+            endTime.push_back(stoi(temp.substr(0, temp.find(","))));
+            temp = temp.substr(temp.find(",") + 1);
+        }
+        line = line.substr(line.find(";") + 1);
+
+        //Summer;true;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
         vector<Season> seasons;
         temp = line.substr(0, line.find(";"));
         line = line.substr(line.find(";") + 1);
         temp.append(",");
-        while (temp.find(',') != string::npos) { 
-            seasons.push_back(stringToSeason(temp.substr(0, temp.find(","))));
+        while (temp.find(',') != string::npos) {
+            seasons.push_back(eh.stringToSeason(temp.substr(0, temp.find(","))));
             temp = temp.substr(temp.find(",") + 1);
         }
-        //Spring,Summer,Fall,Winter;GIOcean;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
 
-        vector<Season> specialSeasons;
-        temp = line.substr(0, line.find(";"));
-        line = line.substr(line.find(";") + 1);
-        temp.append(",");
-        while (temp.find(',') != string::npos){
-            specialSeasons.push_back(stringToSeason(temp.substr(0, temp.find(","))));
-            temp = temp.substr(temp.find(",") + 1);
-        }
-        //GIOcean;Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
-
-        Location specialLocations = stringToLocation(line.substr(0, line.find(";")));
-        line = line.substr(line.find(";") + 1);
         //Sunny;1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
-
         bool rain = true;
         bool sun = true;
         temp = line.substr(0, line.find(";"));
@@ -137,30 +115,34 @@ private:
         if (temp == "Sunny") {
             rain = false;
         }
+
         //1;37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
-
-        int minLength = stoi(line.substr(0, line.find(";")));
+        int minLength = 0;
+        minLength = stoi(line.substr(0, line.find(";")));
         line = line.substr(line.find(";") + 1);
+
         //37;80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
-
-        int maxLength = stoi(line.substr(0, line.find(";")));
+        int maxLength = 0;
+        maxLength = stoi(line.substr(0, line.find(";")));
         line = line.substr(line.find(";") + 1);
+
         //80;Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
-
-        int difficulty = stoi(line.substr(0, line.find(";")));
+        int difficulty = 0;
+        difficulty = stoi(line.substr(0, line.find(";")));
         line = line.substr(line.find(";") + 1);
+
         //Floater;29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
-
-        Behavior behavior = stringToBehavior(line.substr(0, line.find(";")));
+        Behavior behavior = eh.stringToBehavior(line.substr(0, line.find(";")));
         line = line.substr(line.find(";") + 1);
+
         //29;Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
-        
-        int XP = stoi(line.substr(0, line.find(";")));
+        int XP = 0;
+        XP = stoi(line.substr(0, line.find(";")));
         line = line.substr(line.find(";") + 1);
-        //Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
 
+        //Specialty Fish Bundle;Abigail(Loved);Aquatic Research Quest;
         vector<string> usedIn;
-        while(line[0] != '\r') {
+        while (line[0] != '\r') {
             usedIn.push_back(line.substr(0, line.find(";")));
             line = line.substr(line.find(";") + 1);
         }
@@ -168,44 +150,26 @@ private:
         //Aquatic Research Quest;
         //
 
+
         Fish fsh(
-            name, 
-            description, 
-            price, 
-            locations, 
-            startTime, 
-            endTime, 
-            seasons, 
-            specialSeasons, 
-            specialLocations, 
+            name,
+            description,
+            price,
+            locations,
+            note,
+            startTime,
+            endTime,
+            seasons,
             rain,
-            sun, 
-            minLength, 
-            maxLength, 
-            difficulty, 
-            behavior, 
-            XP, 
+            sun,
+            minLength,
+            maxLength,
+            difficulty,
+            behavior,
+            XP,
             usedIn
         );
         return fsh;
     }
 
-    void readAll() {
-        
-        ifstream file("fish_database.csv");
-
-        if (!file.is_open()) {
-            cerr << "Error opening file!" << endl;
-            return; 
-        }
-
-        string line;
-        while (getline(file, line)) {
-            Fish fsh = readFish(line);
-
-            fishList.push_back(fsh);
-        }
-        
-    }
-    
 };
